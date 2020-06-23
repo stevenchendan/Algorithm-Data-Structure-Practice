ff38300dd3acc7958e5aa867e52f37106e1f6583
stevenchendan <qwe16165850@gmail.com>
2020-06-23 18:34:21 +1000

    change to C# code coding style
    bracket start from a new line

--- leetCode/CSharp/28-implement-strStr.cs
+++ leetCode/CSharp/28-implement-strStr.cs
@@ -1,11 +1,16 @@
-public class Solution {
-    public int StrStr(string haystack, string needle) {
-        if (needle == null || haystack == null || needle.Length > haystack.Length) {
+public class Solution 
+{
+    public int StrStr(string haystack, string needle) 
+    {
+        if (needle == null || haystack == null || needle.Length > haystack.Length) 
+        {
             return -1;
         }
         
-        for (int i = 0; i < haystack.Length - needle.Length + 1; i++) {
-            if (haystack.Substring(i, needle.Length) == needle) {
+        for (int i = 0; i < haystack.Length - needle.Length + 1; i++) 
+        {
+            if (haystack.Substring(i, needle.Length) == needle) 
+            {
                 return i;
             }
         }
