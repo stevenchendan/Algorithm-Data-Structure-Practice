87d6877def69910e649ae04d2f95c15bcd36dbdc
stevenchendan <qwe16165850@gmail.com>
2020-11-03 21:29:21 +1100

    add 590 code

---
+++ leetCode/Python/590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            stack.extend(node.children)
+        return result[::-1]
+        
+        
+        
+#         stack, output = [root, ], []
+#         while stack:
+#             root = stack.pop()
+#             if root is not None:
+#                 output.append(root.val)
+#             for c in root.children:
+#                 stack.append(c)
+        
+#         return output[::-1]
+    
+        
