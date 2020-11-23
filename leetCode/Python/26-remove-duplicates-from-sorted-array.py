b3d8ded0558acc72d7a90b8b991b2e17f3a37055
stevenchendan <qwe16165850@gmail.com>
2020-11-23 21:41:09 +1100

    add one more two pointer solution to 26

--- leetCode/Python/26-remove-duplicates-from-sorted-array.py
+++ leetCode/Python/26-remove-duplicates-from-sorted-array.py
@@ -2,16 +2,27 @@
 
 class Solution:
     def removeDuplicates(self, nums: 'List[int]') -> 'int':
+        # solution 1
+        # if not nums:
+        #     return 0
+        # elif len(nums) == 1:
+        #     return 1
+        # index = 0
+        # for num in nums:
+        #     if index < 1 or num != nums[index - 1]:
+        #         nums[index] = num
+        #         index += 1
+        # return index
+
+        # solution 2 two pointer
         if not nums:
-            return 0
-        elif len(nums) == 1:
-            return 1
-        index = 0
-        for num in nums:
-            if index < 1 or num != nums[index - 1]:
-                nums[index] = num
-                index += 1
-        return index
-    
+          return 0
+        i = 0
+        for j in range(1, len(nums)):
+          if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+        return i + 1
+
     
     
