a060f7a962a9f2052aa4efa3f726a663b0574ecf
stevenchendan <qwe16165850@gmail.com>
2020-11-26 22:17:35 +1100

    add binary search solution for question 69

--- leetCode/Python/69-sqrt.py
+++ leetCode/Python/69-sqrt.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/sqrtx/
-
-class Solution:
+# Solution 1: brute force
+class Solution1:
     def mySqrt(self, x: int) -> int:
         i = 0
         while i * i <= x:
@@ -8,3 +8,18 @@ class Solution:
             return i
           else:
             i += 1
+
+# solution 2: binary search
+# time complexity: O(logx)
+# space complexity: O(1)
+class Solution2:
+    def mySqrt(self, x: int) -> int:
+      left, right, result = 0, x, -1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return result
