ed36468cc165cb6db44ff73f8b3590df182c7a2e
stevenchendan <qwe16165850@gmail.com>
2020-11-30 21:48:44 +1100

    add recursive solution to 590

--- leetCode/Python/590-n-ary-tree-postorder-traversal.py
+++ leetCode/Python/590-n-ary-tree-postorder-traversal.py
@@ -12,13 +12,33 @@ class Solution:
         if root is None:
             return []
         
-        stack = [root]
+        # recursive
+        """
+# Definition for a Node.[Photo]
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        
+        # recursive
         result = []
-        while stack:
-            node = stack.pop()
-            result.append(node.val)
-            stack.extend(node.children)
-        return result[::-1]
+        for i in root.children:
+            result.extend(self.postorder(i))
+        return result + [root.val]
+        
+        # stack = [root]
+        # result = []
+        # while stack:
+        #     node = stack.pop()
+        #     result.append(node.val)
+        #     stack.extend(node.children)
+        # return result[::-1]
         
         
         
@@ -29,7 +49,4 @@ class Solution:
 #                 output.append(root.val)
 #             for c in root.children:
 #                 stack.append(c)
-        
 #         return output[::-1]
-    
-        
