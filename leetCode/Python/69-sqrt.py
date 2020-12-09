7e9956d5cd41f0e36c1227d1bacb7c6b2479e793
stevenchendan <qwe16165850@gmail.com>
2020-12-09 21:45:14 +1100

    update question 69 solution

--- leetCode/Python/69-sqrt.py
+++ leetCode/Python/69-sqrt.py
@@ -15,11 +15,42 @@ class Solution1:
 class Solution2:
     def mySqrt(self, x: int) -> int:
       left, right, result = 0, x, -1
+      while left <= right:
+          mid = (left + right) // 2
+          if mid * mid <= x:
+              result = mid
+              left = mid + 1
+          else:
+              right = mid - 1
+      return result
+
+# solution 3: binary search but more understandable
+# time complexity: O(logx)
+# space complexity: O(1)
+class Solution3:
+    def mySqrt(self, x: int) -> int:
+        if x < 0:
+            return null
+        left, right, result = 0, x, -1
         while left <= right:
-            mid = (left + right) // 2
-            if mid * mid <= x:
-                result = mid
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
                 left = mid + 1
-            else:
+            elif mid * mid > x:
                 right = mid - 1
-        return result
+        return right
+
+
+#Solution 4: newton
+        if x < 0:
+            raise Exception("Cannot be negative")
+        if x == 0:
+            return 0
+        cur = 1
+        while True:
+            pre = cur
+            cur = (cur + x / cur) / 2
+            if abs(cur - pre) < 1e-6:
+                return int(cur)
