8f7be3dfa8d07ad2d50826051c22fa617b988f9c
stevenchendan <qwe16165850@gmail.com>
2021-09-22 21:34:51 +1000

    update question 26

--- leetCode/Python/26-remove-duplicates-from-sorted-array.py
+++ leetCode/Python/26-remove-duplicates-from-sorted-array.py
@@ -15,14 +15,24 @@ class Solution:
         # return index
 
         # solution 2 two pointer
-        if not nums:
-          return 0
-        i = 0
-        for j in range(1, len(nums)):
-          if nums[j] != nums[i]:
-            i += 1
-            nums[i] = nums[j]
-        return i + 1
+        # if not nums:
+        #   return 0
+        # i = 0
+        # for j in range(1, len(nums)):
+        #   if nums[j] != nums[i]:
+        #     i += 1
+        #     nums[i] = nums[j]
+        # return i + 1
 
-    
+        # solution 3: two pointer
+        if not nums:
+            return 0
+        length = len(nums)
+        fast = slow = 1
+        while fast < length:
+            if nums[fast] != nums[fast - 1]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
     
