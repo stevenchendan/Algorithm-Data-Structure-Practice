ca1520c86975d5431e60c3d7da1b4aa05845f329
stevenchendan <qwe16165850@gmail.com>
2021-09-24 00:25:36 +1000

    update question 21

--- leetCode/Python/21-merge-two-sorted-lists.py
+++ leetCode/Python/21-merge-two-sorted-lists.py
@@ -30,3 +30,21 @@ class Solution:
         #     current = current.next
         # current.next = l1 or l2
         # return dummy.next
+
+        # in-place, iteratively        
+        # if None in (l1, l2):
+        #     return l1 or l2
+        # dummy = cur = ListNode(0)
+        # dummy.next = l1
+        # while l1 and l2:
+        #     if l1.val < l2.val:
+        #         l1 = l1.next
+        #     else:
+        #         nxt = cur.next
+        #         cur.next = l2
+        #         tmp = l2.next
+        #         l2.next = nxt
+        #         l2 = tmp
+        #     cur = cur.next
+        # cur.next = l1 or l2
+        # return dummy.next
