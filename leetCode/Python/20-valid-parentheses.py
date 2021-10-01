04285bae3521ee9459120aa0233bad63451c9a15
stevenchendan <qwe16165850@gmail.com>
2021-10-01 23:17:53 +1000

    update 20 solution to make it more understandable

--- leetCode/Python/20-valid-parentheses.py
+++ leetCode/Python/20-valid-parentheses.py
@@ -1,15 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # stack and dictionary solution
-        parentheses = {')':'(', ']':'[', '}':'{'}
+        # second time
+        if not s or len(s) % 2 != 0:
+            return False
+        parentheses = {')':'(', '}':'{', ']':'['}
         seen = []
         for char in s:
-            # adding to seen stack if it is is in key value
             if char in parentheses.values():
                 seen.append(char)
             elif char in parentheses.keys():
-                if seen == [] or parentheses[char] != seen.pop():
+                if seen == [] or parentheses[char] != seen[-1]:
                     return False
+                seen.pop()
             else:
                 return False
-        return seen == []
+        return len(seen) == 0
+
+
+    
