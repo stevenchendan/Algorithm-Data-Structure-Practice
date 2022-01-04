e4f907a9b72f5f2e2dbedf718b539ce110002ec9
stevenchendan <qwe16165850@gmail.com>
2022-01-04 22:18:48 +1100

    write question 209 solution and test

---
+++ leetCode/JavaScript/src/questions/209-minumum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -0,0 +1,24 @@
+/**
+ * Question: https://leetcode.com/problems/minimum-size-subarray-sum/
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const minSubArrayLen = function(target, nums) {
+  let result = nums.length + 1;
+  let slow = 0, fast = 0, sum = 0;
+  
+  //slide window
+  while (fast < nums.length) {
+      sum += nums[fast++];
+      
+      while (sum >= target) {
+          result = result < fast - slow ? result : fast - slow;
+          sum -= nums[slow++];
+      }
+  }
+  
+  return result > nums.length ? 0 : result;
+};
+
+export default minSubArrayLen;
---
+++ leetCode/JavaScript/src/questions/209-minumum-size-subarray-sum/__test__/209-minimum-size-subarray-sum.test.js
@@ -0,0 +1,11 @@
+import minSubArrayLen from '../209-minimum-size-subarray-sum'
+
+
+describe("minimal length of a contiguous subarray", () => {
+  it("return the minimal length of a contiguous subarray", () => {
+    expect(minSubArrayLen(7, [2,3,1,2,4,3])).toEqual(2);
+  });
+  it("if no result then return 0", () => {
+    expect(minSubArrayLen(0, [2,3,1,2,4,3])).toEqual(0);
+  });
+});
