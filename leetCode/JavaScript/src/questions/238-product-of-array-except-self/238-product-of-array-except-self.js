a9a0ecdf7899db57d476f13272f6b0dc3f575f85
stevenchendan <qwe16165850@gmail.com>
2022-01-04 23:29:53 +1100

    add 238 solution

---
+++ leetCode/JavaScript/src/questions/238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/product-of-array-except-self/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ const productExceptSelf = function(nums) {
+  if (!nums || nums.length == 0) {
+      return [];
+  }
+  
+  let rightMultipleResult = 1;
+  const result = [];
+  
+  result[0] = 1;
+  
+  //from left to right
+  for (let i = 1; i < nums.length; i++) {
+      result[i] = result[i - 1] * nums[i - 1];
+  }
+  //from right to left
+  for (let j = nums.length - 1; j >= 0; j--) {
+      result[j] = rightMultipleResult * result[j]
+      rightMultipleResult = nums[j] * rightMultipleResult;
+  }
+  
+  return result
+};
+
+export default productExceptSelf;
