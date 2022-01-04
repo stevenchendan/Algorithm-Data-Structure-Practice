ab5cf4eb2d1984a71a246b39552f5eed29ab64bf
stevenchendan <qwe16165850@gmail.com>
2022-01-05 02:13:58 +1100

    add link to description and remove second function name

--- leetCode/JavaScript/src/questions/70-climbing-stairs/70-climbing-stairs.js
+++ leetCode/JavaScript/src/questions/70-climbing-stairs/70-climbing-stairs.js
@@ -1,13 +1,15 @@
 /**
+ * https://leetcode.com/problems/climbing-stairs/
  * @param {number} n
  * @return {number}
  */
-const climbStairs = function calculateHomeManyWaysClimbStairs(n) {
+const climbStairs = function (n) {
   if (n < 0) {
     return -1;
   }
 
   const dp = [1, 1];
+
   for (let i = 2; i <= n; i++) {
     dp[i] = dp[i - 2] + dp[i - 1];
   }
