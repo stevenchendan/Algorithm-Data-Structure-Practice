fe21f1fda4d352d0f27c6655ca341e92b23a3169
stevenchendan <qwe16165850@gmail.com>
2022-01-05 00:27:04 +1100

    add 257 and 236 solution

---
+++ leetCode/JavaScript/src/questions/257-binary-tree-paths/257-binary-tree-paths.js
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/binary-tree-paths/
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+ var binaryTreePaths = function(root) {
+  if (!root) {
+      return root;
+  }
+  
+  //dfs
+  let result = [];
+  const getPath = function (node, path) {
+      if (node.left === null && node.right === null) {
+          path += node.val;
+          result.push(path);
+          return;
+      }
+      path += node.val + '->';
+      if (!!node.left) {
+          getPath(node.left, path);
+      }
+      if (!!node.right) {
+          getPath(node.right, path);
+      }
+  }
+  
+  getPath(root, '');
+  return result;
+};
